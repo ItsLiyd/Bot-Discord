b94b8ee5a3b97ce3cbf6373514dfa70792e4eeb8
ItsLiyd <akunyutubno3@gmail.com>
2025-10-06 13:59:23 +0700

    Update welcomer.js

--- All Commands/auto-message/welcomer.js
+++ All Commands/auto-message/welcomer.js
@@ -5,25 +5,25 @@ module.exports = (client) => {
     // Listener untuk event ketika member baru bergabung ke server
     client.on('guildMemberAdd', async member => {
         
-        const welcomeChannelId = '1357283129911672979'; // Ganti dengan ID channel selamat datang
-        const rulesChannelId = '1357283130280644648';     // Ganti dengan ID channel peraturan
-        const bannerImageUrl = 'https://i.supaimg.com/73f3dd6c-fd74-4590-88b4-439c26f4f125.jpg';   // Ganti dengan URL gambar banner kamu
-        const devUsername = 'Pesatir_Handal';  // Ganti dengan username developer bot
+        const welcomeChannelId = 'CHANNEL_ID_LU'; // Ganti dengan ID channel selamat datang
+        const rulesChannelId = 'CHANNEL_BUAT_RULES_MU';     // Ganti dengan ID channel peraturan
+        const bannerImageUrl = 'URL_GAMBAR';   // Ganti dengan URL gambar banner kamu
+        const devUsername = 'NAMA_DEVOLOPER_BOT';  // Ganti dengan username developer bot
 
         const channel = member.guild.channels.cache.get(welcomeChannelId);
 
         if (!channel) return;
 
         const welcomeEmbed = new EmbedBuilder()
-            .setColor('#FF0000')
-            .setTitle('NEW MEMBER')
+            .setColor('#FF0000') // warna merah
+            .setTitle('NEW MEMBER') // title buat pesan embed lu 
             .setDescription(
             `Welcome <@${member.user.id}> to **${member.guild.name}**!` +
             `\nJangan lupa patuhi <#${rulesChannelId}> agar kamu tidak kena masalah 👍`
             )
             .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
             .setImage(bannerImageUrl)
-            .setFooter({ text: `made with love by [${devUsername}] • Today at ${new Date().toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit' })}` })
+            .setFooter({ text: `made with love by [${devUsername}] • Today at ${new Date().toLocaleTimeString('id-ID', { hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Jakarta' })}` })
             .setTimestamp();
 
         // Kirim pesan selamat datang
@@ -31,4 +31,5 @@ module.exports = (client) => {
             embeds: [welcomeEmbed] 
         }).catch(err => console.error('Gagal mengirim welcome:', err));
     });
+
 };
