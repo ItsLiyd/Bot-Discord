f86f570b53a5a17e2d75c8323aa73311759682a2
ItsLiyd <akunyutubno3@gmail.com>
2025-11-21 21:14:56 +0700

    Create auto-chat.js

---
+++ Main Code/auto-message/auto-chat.js
@@ -0,0 +1,36 @@
+const cron = require('node-cron');
+
+module.exports = (client) => {
+    // Ambil Channel ID dan Timezone dari .env
+    const channelId = process.env.CHAT_CHANNEL_ID; 
+    const timezone = process.env.CRON_TIMEZONE || "Asia/Jakarta"; 
+    
+    // --- Taroh message lu di sini ---
+    const messageContent = 'hai gusy, subskrep Pesatir_Handal '; // <--- taroh sini bjir
+    // ---------------------------------------------
+
+    // ┌─────────────── menit (0 - 59)
+    // │ ┌───────────── jam (0 - 23)
+    // │ │ ┌─────────── hari dalam sebulan (1 - 31)
+    // │ │ │ ┌───────── bulan (1 - 12)
+    // │ │ │ │ ┌─────── hari dalam seminggu (0 - 6, di mana 0 adalah Minggu)
+    // │ │ │ │ │
+    // * * * * *
+
+    cron.schedule('* * * * *', async () => { // Ganti dengan jadwal bg
+        try {
+            const channel = await client.channels.fetch(channelId);
+            if (channel) {
+                // Gunakan messageContent yang sudah didefinisikan di atas
+                channel.send(messageContent); 
+                console.log('Pesan otomatis berhasil dikirim.');
+            } else {
+                console.error('Channel ga ditemuin njir 😹.');
+            }
+        } catch (error) {
+            console.error('woi njer, ada kesalahan tuh 😁:', error);
+        }
+    }, {
+        timezone: timezone 
+    });
+};
